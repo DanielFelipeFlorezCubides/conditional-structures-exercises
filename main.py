41d3813d1ae8894e5c65f2462602044e28656e2d
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-10-31 12:46:50 -0500

    Solucion ejercicio edad en progreso

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-# Escriba un programa que simule una calculadora básica, este puede realizar operación de suma, resta, multiplicación y división. 
-# El programa debe recibir como entrada 2 números reales y un operador, que puede ser +, -, * o /. 
-# La salida del programa debe ser el resultado de la operación.
+# Escriba un programa que entregue la edad del usuario a partir de su fecha de nacimiento:
+# El programa debe tener en cuenta si el cumpleaños ingresado ya pasó durante este año, o si todavía no ocurre.
 
-firstNumber = float(input('Type a number: '))
-secondNumber = float(input('Type another one: '))
-operation = input('Which operation do you wish to do? (+ , - , * , /): ')
+day = int(input('Type your birth day number: '))
+month = int(input('Type your birth month number: '))
+year = int(input('Type your birth year number: '))
 
-if operation == '+':
-    result  = firstNumber + secondNumber
-    print(f'The result is: {result}')
-elif operation == '-':
-    result = firstNumber - secondNumber
-    print(f'The result is: {result}')
-elif operation == '*':
-    result = firstNumber * secondNumber
-    print(f'The result is: {result}')
-else:
-    result = firstNumber / secondNumber
-    print(f'The result is: {result}')
+currentDay = int(input('Type the current day number: '))
+currentMonth = int(input('Type the current month number: '))
+currentYear = int(input('Type the current year number: '))
+
+old = currentYear - year
+print(f'You are {old} years old')
